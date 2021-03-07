2bdd6940b5f5de0e4b6756411a5274ef8685eaa1
Taeuk Gwak <gwak2837@kakao.com>
2021-03-07 17:39:27 +0900

    Add icon on navigation

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,3 +1,8 @@
+import AssignmentTwoToneIcon from '@material-ui/icons/AssignmentTwoTone'
+import HomeRoundedIcon from '@material-ui/icons/HomeRounded'
+import InsertEmoticonTwoToneIcon from '@material-ui/icons/InsertEmoticonTwoTone'
+import ListAltTwoTone from '@material-ui/icons/ListAltTwoTone'
+import StoreRoundedIcon from '@material-ui/icons/StoreRounded'
 import Link from 'next/link'
 import { HEADER_HEIGHT, TABLET_MIN_WIDTH } from 'src/models/constants'
 import styled from 'styled-components'
@@ -8,48 +13,85 @@ const FixedHeader = styled.header`
   bottom: 0;
   transform: translateX(-50%);
 
-  width: ${TABLET_MIN_WIDTH};
+  width: min(100%, ${TABLET_MIN_WIDTH});
   height: ${HEADER_HEIGHT};
   z-index: 1;
 
   background-color: #fff;
 `
 
-const StyledNav = styled.nav`
-  height: 100%;
-`
-
-const FlexContainer = styled.div`
+const FlexContainerAroundNav = styled.nav`
   display: flex;
-  justify-content: space-between;
+  justify-content: space-around;
   align-items: center;
 
   height: 100%;
+
+  > a {
+    flex-grow: 1;
+    height: 100%;
+  }
+`
+
+const FlexContainerCenterCenter = styled.div`
+  display: flex;
+  flex-flow: column nowrap;
+  justify-content: center;
+  align-items: center;
+
+  height: 100%;
+`
+
+const NoMarginH6 = styled.h5`
+  margin: 0;
 `
 
 function Header() {
   const username = 'username'
   return (
     <FixedHeader>
-      <StyledNav>
-        <FlexContainer>
-          <Link href="/">
-            <a href="/">홈</a>
-          </Link>
-          <Link href="/feed">
-            <a href="/feed">피드</a>
-          </Link>
-          <Link href="/stores">
-            <a href="/stores">찜 · 단골 가게</a>
-          </Link>
-          <Link href={`/users/${username}/orders`}>
-            <a href={`/users/${username}/orders`}>주문 내역</a>
-          </Link>
-          <Link href={`/users/${username}`}>
-            <a href={`/users/${username}`}>내 Sindy</a>
-          </Link>
-        </FlexContainer>
-      </StyledNav>
+      <FlexContainerAroundNav>
+        <Link href="/">
+          <a href="/">
+            <FlexContainerCenterCenter>
+              <HomeRoundedIcon fontSize="large" />
+              <NoMarginH6>홈</NoMarginH6>
+            </FlexContainerCenterCenter>
+          </a>
+        </Link>
+        <Link href="/feed">
+          <a href="/feed">
+            <FlexContainerCenterCenter>
+              <ListAltTwoTone fontSize="large" />
+              <NoMarginH6>피드</NoMarginH6>
+            </FlexContainerCenterCenter>
+          </a>
+        </Link>
+        <Link href="/stores">
+          <a href="/stores">
+            <FlexContainerCenterCenter>
+              <StoreRoundedIcon fontSize="large" />
+              <NoMarginH6>찜·단골 매장</NoMarginH6>
+            </FlexContainerCenterCenter>
+          </a>
+        </Link>
+        <Link href={`/users/${username}/orders`}>
+          <a href={`/users/${username}/orders`}>
+            <FlexContainerCenterCenter>
+              <AssignmentTwoToneIcon fontSize="large" />
+              <NoMarginH6>주문 내역</NoMarginH6>
+            </FlexContainerCenterCenter>
+          </a>
+        </Link>
+        <Link href={`/users/${username}`}>
+          <a href={`/users/${username}`}>
+            <FlexContainerCenterCenter>
+              <InsertEmoticonTwoToneIcon fontSize="large" />
+              <NoMarginH6>내 Sindy</NoMarginH6>
+            </FlexContainerCenterCenter>
+          </a>
+        </Link>
+      </FlexContainerAroundNav>
     </FixedHeader>
   )
 }

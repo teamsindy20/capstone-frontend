f3da1ef16d9c58cfbab551f05cac1bdf27ebd3bb
Taeuk Gwak <gwak2837@kakao.com>
2021-03-07 17:39:33 +0900

    Update PageLayout.tsx

--- src/components/layouts/PageLayout.tsx
+++ src/components/layouts/PageLayout.tsx
@@ -1,8 +1,13 @@
 import { ReactNode } from 'react'
-import { HEADER_HEIGHT } from 'src/models/constants'
+import { HEADER_HEIGHT, TABLET_MIN_WIDTH } from 'src/models/constants'
 import styled from 'styled-components'
 import Footer from '../Footer'
 import Header from '../Header'
+
+const MaxWidth = styled.div`
+  max-width: ${TABLET_MIN_WIDTH};
+  margin: 0 auto;
+`
 
 const Padding = styled.div`
   padding: ${HEADER_HEIGHT} 0 0;
@@ -15,7 +20,9 @@ type Props = {
 function PageLayout({ children }: Props) {
   return (
     <>
-      <main>{children}</main>
+      <main>
+        <MaxWidth>{children}</MaxWidth>
+      </main>
       <Padding />
       <Header />
     </>

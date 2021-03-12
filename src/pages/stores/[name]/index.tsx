bbc5321af9b3a2e202475022ab096c8fd28572d0
Taeuk Gwak <gwak2837@kakao.com>
2021-03-12 18:14:40 +0900

    Modify css layout

--- src/pages/stores/[name]/index.tsx
+++ src/pages/stores/[name]/index.tsx
@@ -1,5 +1,20 @@
+import { useRouter } from 'next/router'
+import PageHead from 'src/components/layouts/PageHead'
+import PageLayout from 'src/components/layouts/PageLayout'
+
+const description = '내가 찜한 매장 또는 단골 매장을 모아서 볼 수 있어요.'
+
 function StoreMenuPage() {
-  return <></>
+  const router = useRouter()
+  const { name } = router.query
+
+  return (
+    <PageHead title="캡스톤디자인 - 매장 메뉴" description={description}>
+      <PageLayout>
+        <>{name}</>
+      </PageLayout>
+    </PageHead>
+  )
 }
 
 export default StoreMenuPage

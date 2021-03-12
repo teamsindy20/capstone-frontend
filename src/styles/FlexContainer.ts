bfee3410ebd48235a6d89e5b6fec5cec51bc7d7f
Taeuk Gwak <gwak2837@kakao.com>
2021-03-12 23:43:01 +0900

    Remove useless variable

--- src/styles/FlexContainer.ts
+++ src/styles/FlexContainer.ts
@@ -1,17 +1,7 @@
 import styled from 'styled-components'
 
-export const FlexContainer = styled.div`
-  display: flex;
-`
-
 export const FlexContainerAlignCenter = styled.div`
   display: flex;
-  align-items: center;
-`
-
-export const FlexContainerCenterCenter = styled.div`
-  display: flex;
-  justify-content: center;
   align-items: center;
 `
 

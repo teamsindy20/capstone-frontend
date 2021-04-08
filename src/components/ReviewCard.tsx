dc15a9095a72de338e2418e597eeeaf444b11bde
Taeuk Gwak <gwak2837@kakao.com>
2021-04-08 17:04:07 +0900

    WIP: Add mypage

---
+++ src/components/ReviewCard.tsx
@@ -0,0 +1,13 @@
+export function ReviewLoadingCard() {
+  return <div />
+}
+
+type Props = {
+  review: any
+}
+
+function ReviewCard({ review }: Props) {
+  return <></>
+}
+
+export default ReviewCard

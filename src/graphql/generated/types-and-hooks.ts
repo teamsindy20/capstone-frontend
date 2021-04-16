00c845518ab9ff2f76634ad0846b1b0ccfd6d5e0
Taeuk Gwak <gwak2837@kakao.com>
2021-04-16 10:21:51 +0900

    Add login, register graphql operation

--- src/graphql/generated/types-and-hooks.ts
+++ src/graphql/generated/types-and-hooks.ts
@@ -5,6 +5,7 @@ export type Maybe<T> = T | null
 export type Exact<T extends { [key: string]: unknown }> = { [K in keyof T]: T[K] }
 export type MakeOptional<T, K extends keyof T> = Omit<T, K> & { [SubKey in K]?: Maybe<T[SubKey]> }
 export type MakeMaybe<T, K extends keyof T> = Omit<T, K> & { [SubKey in K]: Maybe<T[SubKey]> }
+const defaultOptions = {}
 /** All built-in and custom scalars, mapped to their actual values */
 export type Scalars = {
   ID: string
@@ -14,32 +15,6 @@ export type Scalars = {
   Float: number
   /** A date-time string at UTC, such as 2007-12-03T10:15:30Z, compliant with the `date-time` format outlined in section 5.6 of the RFC 3339 profile of the ISO 8601 standard for representation of dates and times using the Gregorian calendar. */
   DateTime: any
-}
-
-export type Mutation = {
-  __typename?: 'Mutation'
-  /** 이메일과 1번 해싱한 비밀번호를 전송하면 인증 토큰을 반환한다. */
-  login?: Maybe<Scalars['String']>
-  /** HTTP Header나 쿠키에 인증 토큰 정보를 넣어서 요청하면 로그아웃 성공 여부를 반환한다. */
-  logout: Scalars['Boolean']
-  createUserTable: Scalars['Boolean']
-}
-
-export type MutationLoginArgs = {
-  email: Scalars['String']
-  passwordHash: Scalars['String']
-}
-
-export type Artist = {
-  __typename?: 'Artist'
-  id: Scalars['ID']
-  name: Scalars['String']
-}
-
-export enum CrawlingSource {
-  Youtube = 'YOUTUBE',
-  Melon = 'MELON',
-  Icezam = 'ICEZAM',
 }
 
 export type Comment = {
@@ -54,56 +29,168 @@ export type Comment = {
   likeCount?: Maybe<Scalars['Int']>
 }
 
-export type Music = {
-  __typename?: 'Music'
-  id: Scalars['ID']
-  creationDate: Scalars['DateTime']
-  title: Scalars['String']
-  artists: Array<Scalars['String']>
-  searchCount: Scalars['Int']
-  albumImage?: Maybe<Scalars['String']>
-  albumColor?: Maybe<Scalars['String']>
-  artistImage?: Maybe<Scalars['String']>
-  genres?: Maybe<Array<Scalars['String']>>
-  lyrics?: Maybe<Array<Scalars['String']>>
-  melonLink?: Maybe<Scalars['String']>
-  shazamId?: Maybe<Scalars['Int']>
-  youtubeLink?: Maybe<Scalars['String']>
-  youtubeImage?: Maybe<Scalars['String']>
-  /** 이 노래를 부른 가수의 다른 노래를 검색 횟수 순으로 반환한다. # 페이지네이션 필요 */
-  artistOtherMusics?: Maybe<Array<Music>>
-  /** 이 노래에 해당하는 댓글 목록을 반환한다. # 페이지네이션 필요 */
-  comments?: Maybe<Array<Comment>>
-  /** 이 노래가 포함된 재생 목록을 반환한다. # 페이지네이션 필요 */
-  includedPlaylists?: Maybe<Array<Playlist>>
-  /** 이 노래와 비슷한 노래 목록을 반환한다. # 페이지네이션 필요 */
-  similarMusics?: Maybe<Array<Music>>
+export enum CrawlingSource {
+  Youtube = 'YOUTUBE',
+  Melon = 'MELON',
+  Icezam = 'ICEZAM',
 }
 
-export type Playlist = {
-  __typename?: 'Playlist'
+export type Menu = {
+  __typename?: 'Menu'
   id: Scalars['ID']
   creationDate: Scalars['DateTime']
+  modificationDate: Scalars['DateTime']
   name: Scalars['String']
-  musics?: Maybe<Array<Music>>
+  price: Scalars['Int']
+  category: Scalars['String']
+  deliciousReviewCount: Scalars['Int']
+  deliciousReviewRatio: Scalars['Int']
+  fineReviewCount: Scalars['Int']
+  fineReviewRatio: Scalars['Int']
+  positiveReviewRatio: Scalars['Int']
+  badReviewCount: Scalars['Int']
+  badReviewRatio: Scalars['Int']
+  newOrderCount: Scalars['Int']
+  newOrderRatio: Scalars['Int']
+  reorderCount: Scalars['Int']
+  reorderRatio: Scalars['Int']
+  newCustomerCount: Scalars['Int']
+  newCustomerRatio: Scalars['Int']
+  regularCustomerCount: Scalars['Int']
+  regularCustomerRatio: Scalars['Int']
+  favoriteCount: Scalars['Int']
+  clickCount: Scalars['Int']
+  storePostCount: Scalars['Int']
+  isDiscounted: Scalars['Boolean']
+  canBePicked: Scalars['Boolean']
+  canBeReserved: Scalars['Boolean']
+  /** from other table */
+  imageUrl?: Maybe<Array<Scalars['String']>>
+  hashtags?: Maybe<Array<Scalars['String']>>
+  bookmark: Scalars['Boolean']
+}
+
+export type Mutation = {
+  __typename?: 'Mutation'
+  /** 회원가입에 필요한 정보를 주면 성공했을 때 인증 토큰을 반환한다. */
+  register: Scalars['String']
+  /** 이메일과 1번 해싱한 비밀번호를 전송하면 인증 토큰을 반환한다. */
+  login: Scalars['String']
+  /** 인증 토큰과 같이 요청하면 로그아웃 성공 여부를 반환한다. */
+  logout: Scalars['Boolean']
+}
+
+export type MutationRegisterArgs = {
+  input: RegisterInput
+}
+
+export type MutationLoginArgs = {
+  email: Scalars['String']
+  passwordHash: Scalars['String']
+}
+
+export type Order = {
+  __typename?: 'Order'
+  id: Scalars['ID']
+  creationDate: Scalars['DateTime']
+  modificationDate: Scalars['DateTime']
+  orderStatus: OrderStatus
+  orderTotal: Scalars['Int']
+  /** from other table */
+  store: Store
+  review?: Maybe<Review>
+  menu?: Maybe<Array<Menu>>
+}
+
+export enum OrderStatus {
+  OrderWaiting = 'ORDER_WAITING',
+  CookingInProgress = 'COOKING_IN_PROGRESS',
+  DeliveryInProgress = 'DELIVERY_IN_PROGRESS',
+  DeliveryCompletion = 'DELIVERY_COMPLETION',
+}
+
+export type Query = {
+  __typename?: 'Query'
+  /** 인증 토큰과 같이 요청하면 사용자 정보를 반환한다. */
+  me?: Maybe<User>
+}
+
+export enum Rating {
+  Delicious = 'DELICIOUS',
+  Good = 'GOOD',
+  Bad = 'BAD',
+}
+
+export type RegisterInput = {
+  email: Scalars['String']
+  passwordHash: Scalars['String']
+  /** nullable */
+  imageUrl?: Maybe<Scalars['String']>
+  name?: Maybe<Scalars['String']>
+  phoneNumber?: Maybe<Scalars['String']>
+  gender?: Maybe<Scalars['String']>
+  birthDate?: Maybe<Scalars['DateTime']>
+  address?: Maybe<Scalars['String']>
+  preference?: Maybe<Array<Scalars['String']>>
+}
+
+export type Review = {
+  __typename?: 'Review'
+  id: Scalars['ID']
+  creationDate: Scalars['DateTime']
+  modificationDate: Scalars['DateTime']
+  helpingOthersCount: Scalars['Int']
+  rating: Rating
+  goodPointContent?: Maybe<Scalars['String']>
+  desiredPointContent?: Maybe<Scalars['String']>
+}
+
+export type Store = {
+  __typename?: 'Store'
+  id: Scalars['ID']
+  creationDate: Scalars['DateTime']
+  modificationDate: Scalars['DateTime']
+  name: Scalars['String']
+  address: Scalars['String']
+  deliveryFee: Scalars['Int']
+  minimumDeliveryAmount: Scalars['Int']
+  deliciousReviewCount: Scalars['Int']
+  fineReviewCount: Scalars['Int']
+  badReviewCount: Scalars['Int']
+  newOrderCount: Scalars['Int']
+  reorderCount: Scalars['Int']
+  newCustomerCount: Scalars['Int']
+  regularCustomerCount: Scalars['Int']
+  favoriteCount: Scalars['Int']
+  clickCount: Scalars['Int']
+  postCount: Scalars['Int']
+  /** nullable */
+  reviewEventContent?: Maybe<Scalars['String']>
+  regularCustomerEventContent?: Maybe<Scalars['String']>
+  deliveryTimeMin?: Maybe<Scalars['Int']>
+  deliveryTimeMax?: Maybe<Scalars['Int']>
 }
 
 export type User = {
   __typename?: 'User'
   id: Scalars['ID']
   creationDate: Scalars['DateTime']
+  modificationDate: Scalars['DateTime']
   email: Scalars['String']
-  token: Scalars['String']
-  name: Scalars['String']
-  age: Scalars['Int']
-}
-
-export type Query = {
-  __typename?: 'Query'
-  /** 사용자 목록을 반환한다. (관리자 전용) */
-  users?: Maybe<Array<User>>
-  /** 내 정보를 반환한다. 해당 권한이 없으면 오류가 발생한다. */
-  me?: Maybe<User>
+  point: Scalars['Int']
+  /** nullable */
+  imageUrl?: Maybe<Scalars['String']>
+  name?: Maybe<Scalars['String']>
+  phoneNumber?: Maybe<Scalars['String']>
+  gender?: Maybe<Scalars['String']>
+  birthDate?: Maybe<Scalars['DateTime']>
+  address?: Maybe<Scalars['String']>
+  /** from other table */
+  favoriteMenu?: Maybe<Array<Menu>>
+  favoriteStores?: Maybe<Array<Store>>
+  regularStores?: Maybe<Array<Store>>
+  orders?: Maybe<Array<Order>>
+  preference?: Maybe<Array<Scalars['String']>>
 }
 
 export type LoginMutationVariables = Exact<{
@@ -113,12 +200,10 @@ export type LoginMutationVariables = Exact<{
 
 export type LoginMutation = { __typename?: 'Mutation' } & Pick<Mutation, 'login'>
 
-export type UsersQueryVariables = Exact<{
-  id: Scalars['ID']
-}>
+export type MeQueryVariables = Exact<{ [key: string]: never }>
 
-export type UsersQuery = { __typename?: 'Query' } & {
-  users?: Maybe<Array<{ __typename?: 'User' } & Pick<User, 'id'>>>
+export type MeQuery = { __typename?: 'Query' } & {
+  me?: Maybe<{ __typename?: 'User' } & Pick<User, 'id' | 'email'>>
 }
 
 export const LoginDocument = gql`
@@ -149,45 +234,46 @@ export type LoginMutationFn = Apollo.MutationFunction<LoginMutation, LoginMutati
 export function useLoginMutation(
   baseOptions?: Apollo.MutationHookOptions<LoginMutation, LoginMutationVariables>
 ) {
-  return Apollo.useMutation<LoginMutation, LoginMutationVariables>(LoginDocument, baseOptions)
+  const options = { ...defaultOptions, ...baseOptions }
+  return Apollo.useMutation<LoginMutation, LoginMutationVariables>(LoginDocument, options)
 }
 export type LoginMutationHookResult = ReturnType<typeof useLoginMutation>
 export type LoginMutationResult = Apollo.MutationResult<LoginMutation>
 export type LoginMutationOptions = Apollo.BaseMutationOptions<LoginMutation, LoginMutationVariables>
-export const UsersDocument = gql`
-  query Users($id: ID!) {
-    users {
+export const MeDocument = gql`
+  query Me {
+    me {
       id
+      email
     }
   }
 `
 
 /**
- * __useUsersQuery__
+ * __useMeQuery__
  *
- * To run a query within a React component, call `useUsersQuery` and pass it any options that fit your needs.
- * When your component renders, `useUsersQuery` returns an object from Apollo Client that contains loading, error, and data properties
+ * To run a query within a React component, call `useMeQuery` and pass it any options that fit your needs.
+ * When your component renders, `useMeQuery` returns an object from Apollo Client that contains loading, error, and data properties
  * you can use to render your UI.
  *
  * @param baseOptions options that will be passed into the query, supported options are listed on: https://www.apollographql.com/docs/react/api/react-hooks/#options;
  *
  * @example
- * const { data, loading, error } = useUsersQuery({
+ * const { data, loading, error } = useMeQuery({
  *   variables: {
- *      id: // value for 'id'
  *   },
  * });
  */
-export function useUsersQuery(
-  baseOptions: Apollo.QueryHookOptions<UsersQuery, UsersQueryVariables>
-) {
-  return Apollo.useQuery<UsersQuery, UsersQueryVariables>(UsersDocument, baseOptions)
+export function useMeQuery(baseOptions?: Apollo.QueryHookOptions<MeQuery, MeQueryVariables>) {
+  const options = { ...defaultOptions, ...baseOptions }
+  return Apollo.useQuery<MeQuery, MeQueryVariables>(MeDocument, options)
 }
-export function useUsersLazyQuery(
-  baseOptions?: Apollo.LazyQueryHookOptions<UsersQuery, UsersQueryVariables>
+export function useMeLazyQuery(
+  baseOptions?: Apollo.LazyQueryHookOptions<MeQuery, MeQueryVariables>
 ) {
-  return Apollo.useLazyQuery<UsersQuery, UsersQueryVariables>(UsersDocument, baseOptions)
+  const options = { ...defaultOptions, ...baseOptions }
+  return Apollo.useLazyQuery<MeQuery, MeQueryVariables>(MeDocument, options)
 }
-export type UsersQueryHookResult = ReturnType<typeof useUsersQuery>
-export type UsersLazyQueryHookResult = ReturnType<typeof useUsersLazyQuery>
-export type UsersQueryResult = Apollo.QueryResult<UsersQuery, UsersQueryVariables>
+export type MeQueryHookResult = ReturnType<typeof useMeQuery>
+export type MeLazyQueryHookResult = ReturnType<typeof useMeLazyQuery>
+export type MeQueryResult = Apollo.QueryResult<MeQuery, MeQueryVariables>

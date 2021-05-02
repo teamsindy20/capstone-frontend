e718b9c7999b967275ed107cb90e0f5f06d48bd5
HyojinKim <gywls00100@gmail.com>
2021-05-02 19:02:58 +0900

    Create FindPW page

---
+++ src/pages/findpw/index.tsx
@@ -0,0 +1,14 @@
+import PageHead from 'src/components/layouts/PageHead'
+import LoginPageLayout from 'src/components/layouts/LoginPageLayout'
+
+function FindPWPage() {
+  return (
+    <PageHead>
+      <LoginPageLayout>
+        <button>비밀번호찾기</button>
+      </LoginPageLayout>
+    </PageHead>
+  )
+}
+
+export default FindPWPage

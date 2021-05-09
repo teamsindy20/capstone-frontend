0c4c4c99eecc37821d2ba9f7c5f2110661c19b78
HyojinKim <gywls00100@gmail.com>
2021-05-09 23:37:26 +0900

    Add store header

--- src/pages/stores/index.tsx
+++ src/pages/stores/index.tsx
@@ -2,6 +2,7 @@ import { useContext } from 'react'
 import { handleApolloError } from 'src/apollo/error'
 import PageHead from 'src/components/layouts/PageHead'
 import PageLayout from 'src/components/layouts/PageLayout'
+import TopHeader from 'src/components/TopHeader'
 import NotLogin from 'src/components/NotLogin'
 import StoreCard from 'src/components/StoreCard'
 import {
@@ -10,6 +11,12 @@ import {
 } from 'src/graphql/generated/types-and-hooks'
 import styled from 'styled-components'
 import { GlobalContext } from '../_app'
+import { FlexContainerAlignCenter } from 'src/styles/FlexContainer'
+import FavoriteRoundedIcon from '@material-ui/icons/FavoriteRounded'
+import grey from '@material-ui/core/colors/grey'
+import red from '@material-ui/core/colors/red'
+
+const StyledFavoriteRoundedIcon = { fontSize: 24, color: grey[800] }
 
 const GridContainerBackground = styled.div`
   display: grid;
@@ -17,6 +24,15 @@ const GridContainerBackground = styled.div`
 
   background: #eee;
   border: 1px solid #eee;
+`
+
+const FlexContainerCenterCenter = styled(FlexContainerAlignCenter)`
+  justify-content: center;
+  height: 100%;
+`
+
+const NoMarginH3 = styled.h3`
+  margin: 0;
 `
 
 const description = '내가 찜한 매장 또는 단골 매장을 모아서 볼 수 있어요.'
@@ -49,7 +65,13 @@ function StoresPage() {
   return (
     <PageHead title="Deple - 찜·단골 매장" description={description}>
       <PageLayout>
-        <h2>찜·단골 매장</h2>
+        <TopHeader>
+          <FlexContainerCenterCenter>
+            <FavoriteRoundedIcon style={StyledFavoriteRoundedIcon} />
+            <NoMarginH3>찜·단골</NoMarginH3>
+          </FlexContainerCenterCenter>
+        </TopHeader>
+
         <h3>단골 매장</h3>
         <GridContainerBackground>
           {regularStores?.map((regularStore) => (

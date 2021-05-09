670aba927a41dab0914b0b1db4187b3da9a13765
HyojinKim <gywls00100@gmail.com>
2021-05-09 22:13:48 +0900

    Modify Postcard layout - gridlayout

--- src/components/PostCard.tsx
+++ src/components/PostCard.tsx
@@ -17,6 +17,8 @@ import { PostsByAddressQuery } from 'src/graphql/generated/types-and-hooks'
 
 const StyledFavoriteRoundedIcon = { fontSize: 20, color: red[500] }
 
+const StyledChatBubbleOutlineRoundedIcon = { fontSize: 20, color: grey[800] }
+
 const SkeletonImageRound = styled(SkeletonGradient)`
   position: relative;
   width: 2rem;
@@ -67,6 +69,46 @@ const NoMarginP = styled.p`
   margin: 0;
 `
 
+const GridInCardColumn2 = styled.div`
+  display: grid;
+  align-items: center;
+  justify-content: center;
+  grid-template-columns: 1fr 1fr;
+  grid-template-rows: auto min-content;
+`
+
+const ImgInCard = styled.img`
+  overflow: hidden;
+  border-radius: 5%;
+
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 100%;
+  height: 270px;
+`
+const TextInCard = styled.div`
+  padding: 20px;
+  font-size: 1rem;
+  overflow: visible scroll;
+  height: 270px;
+`
+
+const Tag = styled.span<{ color: string }>`
+  margin: 10px;
+  padding: 5px 10px;
+  white-space: nowrap;
+  border-radius: 12px;
+  font-size: 14px;
+  text-align: center;
+  vertical-align: middle;
+  background-color: ${(p) => p.color};
+`
+
+const FlexContainerBottomCard = styled.div`
+  display: flex;
+`
+
 export function PostLoadingCard() {
   return (
     <ShadowingLi>
@@ -100,10 +142,35 @@ function PostCard({ post }: Props) {
 
   return (
     <ShadowingLi>
+      <GridInCardColumn2>
+        <div>
+          <StyledImg src={store.imageUrls ? store.imageUrls[0] : ''} alt="store profile" />
+          <NoMarginH5>{store.name}</NoMarginH5>
+        </div>
+
+        <div>
+          <Tag color="rgb(190, 235, 253)">오늘의라인업</Tag>
+        </div>
+        <div>
+          <ImgInCard src={post.imageUrls ? post.imageUrls[0] : ''} alt="post" />
+        </div>
+        <TextInCard>
+          {post.contents.map((content) => (content ? <NoMarginP>{content}</NoMarginP> : <br />))}
+        </TextInCard>
+      </GridInCardColumn2>
+      <FlexContainerBottomCard>
+        <ChatBubbleOutlineRoundedIcon style={StyledChatBubbleOutlineRoundedIcon} />
+        <NoMarginH5>댓글</NoMarginH5>
+        <FavoriteRoundedIcon style={StyledFavoriteRoundedIcon} /> {post.likeCount}
+      </FlexContainerBottomCard>
+      <FlexContainerBottomCard>
+        <input placeholder="댓글을 입력해주세요." />
+      </FlexContainerBottomCard>
+
       {/* <div>
         <img src="https://gramho.com/hosted-by-instagram/url=https%3A%7C%7C%7C%7Cinstagram.fiev22-2.fna.fbcdn.net%7C%7Cv%7C%7Ct51.2885-19%7C%7Cs150x150%7C%7C133784715_672813013398326_5083752991447256061_n.jpg%3Ftp%3D1%26_nc_ht%3Dinstagram.fiev22-2.fna.fbcdn.net%26_nc_ohc%3DkKzoh7bNOMsAX_yPtQp%26edm%3DABfd0MgBAAAA%26ccb%3D7-4%26oh%3Dd3276fb2af511a2070378be742c0d0aa%26oe%3D60BCEDE6%26_nc_sid%3D7bff83"></img>
       </div> */}
-      <FlexContainerBetweenPadding>
+      {/* <FlexContainerBetweenPadding>
         <GridContainerColumn2>
           <div>
             <StyledImg src={store.imageUrls ? store.imageUrls[0] : ''} alt="store profile" />
@@ -148,7 +215,7 @@ function PostCard({ post }: Props) {
           </li>
         </ul>
         <input placeholder="댓글 달기..." />
-      </GridContainerPadding>
+      </GridContainerPadding> */}
     </ShadowingLi>
   )
 }

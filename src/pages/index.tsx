3a9c4d2918d740d2b02da6b2b2ae003180c8ac0d
Taeuk Gwak <gwak2837@kakao.com>
2021-05-17 10:19:27 +0900

    Fix ui error
    메뉴 찜 ui 일관성 오류 수정

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -194,6 +194,7 @@ function HomePage() {
   const [onlyImage, toggleOnlyImage] = useBoolean(false)
 
   const menusQueryResult = useMenusQuery({
+    fetchPolicy: 'cache-and-network',
     notifyOnNetworkStatusChange: true,
     onError: handleApolloError,
   })

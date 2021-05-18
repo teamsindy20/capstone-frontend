2184c2ed54461da4505e4f068827ec5691dd9005
Taeuk Gwak <gwak2837@kakao.com>
2021-05-18 18:09:13 +0900

    Add button of removing all menus in cart

--- src/pages/cart/index.tsx
+++ src/pages/cart/index.tsx
@@ -2,7 +2,7 @@ import PageHead from 'src/components/layouts/PageHead'
 import useGoToPage from 'src/hooks/useGoToPage'
 import TopHeader from 'src/components/TopHeader'
 import { useReactiveVar } from '@apollo/client'
-import { cartMenusVar } from 'src/apollo/cache'
+import { cartMenusVar, setCartMenus } from 'src/apollo/cache'
 import CartMenuCard from 'src/components/CartMenuCard'
 
 function CartPage() {
@@ -12,13 +12,15 @@ function CartPage() {
 
   return (
     <PageHead>
-      <TopHeader>장바구니</TopHeader>
+      <TopHeader>
+        장바구니 <button onClick={() => setCartMenus([])}>전체삭제</button>
+      </TopHeader>
 
       {cartMenus.map((cartMenu) => (
         <CartMenuCard key={cartMenu.id} menu={cartMenu} />
       ))}
 
-      <button onClick={goToOrderPage}>결제하기</button>
+      <button onClick={goToOrderPage}>주문하기</button>
     </PageHead>
   )
 }

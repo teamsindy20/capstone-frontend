893d8b15138e6d0c4731c0fce66cbd6282143c2e
Taeuk Gwak <gwak2837@kakao.com>
2021-05-18 12:31:48 +0900

    Create cart page

--- src/pages/cart/index.tsx
+++ src/pages/cart/index.tsx
@@ -1,7 +1,14 @@
 import PageHead from 'src/components/layouts/PageHead'
+import useGoToPage from 'src/hooks/useGoToPage'
 
 function CartPage() {
-  return <PageHead>메뉴 장바구니 페이지</PageHead>
+  const goToOrderPage = useGoToPage('/order')
+
+  return (
+    <PageHead>
+      메뉴 장바구니 페이지<button onClick={goToOrderPage}>결제하기</button>
+    </PageHead>
+  )
 }
 
 export default CartPage

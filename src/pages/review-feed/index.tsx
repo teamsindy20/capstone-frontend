af76590f83d00012a1fbabbc82f82af9c33f0e61
Taeuk Gwak <gwak2837@kakao.com>
2021-05-19 16:40:27 +0900

    Remove useless codes

--- src/pages/review-feed/index.tsx
+++ src/pages/review-feed/index.tsx
@@ -1,49 +1,10 @@
-import { useState } from 'react'
-import useInfiniteScroll from 'react-infinite-scroll-hook'
-import { handleApolloError } from 'src/apollo/error'
+import { Tabs } from 'antd'
+import { useRouter } from 'next/router'
 import PageHead from 'src/components/layouts/PageHead'
 import PageLayout from 'src/components/layouts/PageLayout'
-import PostCard, { PostLoadingCard } from 'src/components/PostCard'
-import { usePostsByAddressQuery } from 'src/graphql/generated/types-and-hooks'
-import { sleep } from 'src/utils/commons'
-import styled from 'styled-components'
 import TopHeader from 'src/components/TopHeader'
-import StoreRoundedIcon from '@material-ui/icons/StoreRounded'
-import grey from '@material-ui/core/colors/grey'
-import { FlexContainerAlignCenter } from 'src/styles/FlexContainer'
-import { Tabs } from 'antd'
-import StoresFeedPage from 'src/pages/feed'
-import { useRouter } from 'next/router'
-
-const PADDING_TOP = '3rem'
-
-const BORDER_HEIGHT = '2px'
-
-const StyledStoreRoundedIcon = { fontSize: 28, color: grey[800] }
-
-const GridContainerUl = styled.ul`
-  display: grid;
-  gap: 3rem;
-`
 
 const description = '다른 사람이 작성한 리뷰를 읽어보세요.'
-
-const TopContainer = styled.div`
-  width: 100%;
-  height: 60px;
-  background-color: #fff5f5;
-  margin-bottom: 10px;
-  text-align: center;
-`
-
-const FlexContainerCenterCenter = styled(FlexContainerAlignCenter)`
-  justify-content: center;
-  height: 100%;
-`
-
-const NoMarginH3 = styled.h3`
-  margin: 0;
-`
 
 function FeedPage() {
   const router = useRouter()

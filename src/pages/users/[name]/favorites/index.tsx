0dcf2f772412458b5de878fac26ac70928e4d964
Taeuk Gwak <gwak2837@kakao.com>
2021-05-19 15:53:39 +0900

    Split favorite page

--- src/pages/users/[name]/favorites/index.tsx
+++ src/pages/users/[name]/favorites/index.tsx
@@ -8,6 +8,7 @@ import StoreCard from 'src/components/StoreCard'
 import {
   useFavoriteMenusQuery,
   useFavoriteStoresQuery,
+  useMenuLazyQuery,
 } from 'src/graphql/generated/types-and-hooks'
 import styled from 'styled-components'
 import { GlobalContext } from '../../../_app'
@@ -20,6 +21,7 @@ import { sleep } from 'src/utils/commons'
 import MenuCard, { MenuLoadingCard } from 'src/components/MenuCard'
 import { PhotoOnlyButton, GridContainerUl } from 'src/pages'
 import { Tabs } from 'antd'
+import { useRouter } from 'next/router'
 
 const StyledFavoriteRoundedIcon = { fontSize: 22, color: grey[800] }
 
@@ -45,95 +47,70 @@ const description = '내가 찜한 매장 및 찜한 메뉴를 모아서 볼 수
 function UserFavoritesPage() {
   const { user, loading } = useContext(GlobalContext)
 
-  const [hasMoreMenus, setHasMoreMenus] = useState(true)
   const [onlyImage, toggleOnlyImage] = useBoolean(false)
-
-  const favoriteMenusQueryResult = useFavoriteMenusQuery({
-    fetchPolicy: 'cache-and-network',
-    onError: handleApolloError,
-    skip: !user,
-  })
-
-  const favoriteMenus = favoriteMenusQueryResult.data?.me.favoriteMenus
-
-  async function fetchMoreMenus() {
-    if (favoriteMenus?.length) {
-      await sleep(5000) // fetchMore({ variables: { from, count } })
-      setHasMoreMenus(false)
-    } else {
-      setHasMoreMenus(false)
-    }
-  }
-
-  const [sentryRef] = useInfiniteScroll({
-    loading: favoriteMenusQueryResult.loading,
-    hasNextPage: hasMoreMenus,
-    onLoadMore: fetchMoreMenus,
-  })
-
   const [hasMoreStores, setHasMoreStores] = useState(true)
 
-  const favoriteStoresQueryResult = useFavoriteStoresQuery({
+  const { data, loading: isFavoriteStoresLoading } = useFavoriteStoresQuery({
     fetchPolicy: 'cache-and-network',
     onError: handleApolloError,
     skip: !user,
   })
 
-  const favoriteStores = favoriteStoresQueryResult.data?.me.favoriteStores
+  const favoriteStores = data?.me.favoriteStores
 
   async function fetchMoreStores() {
     if (favoriteStores?.length) {
       await sleep(5000) // fetchMore({ variables: { from, count } })
-      setHasMoreMenus(false)
+      setHasMoreStores(false)
     } else {
-      setHasMoreMenus(false)
+      setHasMoreStores(false)
+    }
+  }
+
+  const [sentryRef] = useInfiniteScroll({
+    loading: isFavoriteStoresLoading,
+    hasNextPage: hasMoreStores,
+    onLoadMore: fetchMoreStores,
+  })
+
+  const router = useRouter()
+
+  function goToPage(activeKey: string) {
+    switch (activeKey) {
+      case 'favoriteMenus':
+      case 'favoriteStores':
+        return `/users/${router.query.name}/${activeKey}`
+      default:
+        return ''
     }
   }
 
   if (!user) {
     return (
-      <PageHead title="디저트핏 - 찜" description={description}>
+      <PageHead title="디저트핏 - 찜 매장" description={description}>
         <PageLayout>{loading ? 'user loading...' : <NotLogin />}</PageLayout>
       </PageHead>
     )
   }
 
   return (
-    <PageHead title="디저트핏 - 찜" description={description}>
+    <PageHead title="디저트핏 - 찜 매장" description={description}>
       <PageLayout>
-        <Tabs defaultActiveKey="1" centered>
-          <Tabs.TabPane tab="메뉴" key="1">
-            <div>
-              {/* <button onClick={() => favoriteMenusQueryResult.refetch()}>refetch</button> */}
-              <PhotoOnlyButton onClick={toggleOnlyImage}>Photo Only</PhotoOnlyButton>
-              <GridContainerUl onlyImage={onlyImage}>
-                {favoriteMenus?.map((favoriteMenu) => (
-                  <MenuCard
-                    key={favoriteMenu.id}
-                    afterPickingMenu={() => favoriteMenusQueryResult.refetch()}
-                    menu={favoriteMenu}
-                    onlyImage={onlyImage}
-                  />
-                ))}
-              </GridContainerUl>
-              {loading || hasMoreMenus ? (
-                <div ref={sentryRef}>
-                  <MenuLoadingCard onlyImage={onlyImage} />
-                </div>
-              ) : (
-                !favoriteMenus?.length && <h4>찜한 메뉴가 없어요</h4>
-              )}
-            </div>
-          </Tabs.TabPane>
-          <Tabs.TabPane tab="매장" key="2">
-            <GridContainerBackground>
-              {favoriteStores?.map((regularStore) => (
-                <StoreCard key={regularStore.id} store={regularStore} />
-              ))}
-              {!favoriteStores?.length && <h4>찜한 매장이 없어요</h4>}
-            </GridContainerBackground>
-          </Tabs.TabPane>
+        <Tabs
+          defaultActiveKey="favoriteStores"
+          centered
+          onTabClick={(activeKey) => router.push(goToPage(activeKey))}
+        >
+          <Tabs.TabPane tab="메뉴" key="favoriteMenus" />
+          <Tabs.TabPane tab="매장" key="favoriteStores" />
         </Tabs>
+
+        <GridContainerBackground>
+          {favoriteStores?.map((regularStore) => (
+            <StoreCard key={regularStore.id} store={regularStore} />
+          ))}
+          {!favoriteStores?.length && <h4>찜한 매장이 없어요</h4>}
+        </GridContainerBackground>
       </PageLayout>
     </PageHead>
   )

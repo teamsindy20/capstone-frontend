0b221f206e234c2a753fb8cb29de28c16e3ed48a
HyojinKim <gywls00100@gmail.com>
2021-05-26 22:20:13 +0900

    Modify search, select Modify margin

--- src/pages/stores/[name]/reviews/index.tsx
+++ src/pages/stores/[name]/reviews/index.tsx
@@ -2,12 +2,19 @@ import { Tabs, Input, Space, Select, Divider, Row, Col } from 'antd'
 import { useRouter } from 'next/router'
 import PageHead from 'src/components/layouts/PageHead'
 import PageLayout from 'src/components/layouts/PageLayout'
+import styled from 'styled-components'
+import { FlexContainerAlignCenter, FlexContainerBetween } from '../../../../styles/FlexContainer'
+import ReviewCard from '../../../../components/ReviewCard'
 
 const description = '매장에서 판매하는 메뉴의 리뷰를 확인해보세요.'
 
 const { Search } = Input
 
 const { Option } = Select
+
+const MarginDiv = styled.div`
+  margin: 0.5rem;
+`
 
 const onSearch = (value: any) => console.log(value)
 
@@ -44,20 +51,23 @@ function StoreReviewsPage() {
         </Tabs>
         <div>리뷰 페이지</div>
         <div>메뉴 검색: {router.query.menu}</div>
-        <Space direction="horizontal">
-          <Search
-            placeholder="내용을 입력해주세요."
-            allowClear
-            onSearch={onSearch}
-            style={{ width: 200 }}
-          />
-          <Select defaultValue="lucy" style={{ width: 120 }} onChange={handleChange}>
-            <Option value="like">좋아요순</Option>
-            <Option value="reorder">재주문율순</Option>
-            <Option value="date">최신순</Option>
-          </Select>
-        </Space>
-        <Divider />
+        <MarginDiv>
+          <FlexContainerBetween>
+            <Search
+              placeholder="내용을 입력해주세요."
+              allowClear
+              onSearch={onSearch}
+              style={{ width: 230 }}
+            />
+            <Select defaultValue="like" style={{ width: 120 }} onChange={handleChange}>
+              <Option value="like">좋아요순</Option>
+              <Option value="reorder">재주문율순</Option>
+              <Option value="date">최신순</Option>
+            </Select>
+          </FlexContainerBetween>
+          <Divider />
+          <ReviewCard />
+        </MarginDiv>
       </PageLayout>
     </PageHead>
   )

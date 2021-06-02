d97f424b9cd9fd67b59a1652ed143185e7c8242e
Taeuk Gwak <gwak2837@kakao.com>
2021-06-02 11:36:13 +0900

    Modify global css

--- src/models/constants.ts
+++ src/models/constants.ts
@@ -1,5 +1,5 @@
 export const MOBILE_MIN_WIDTH = '280px'
-export const TABLET_MIN_WIDTH = '640px'
+export const TABLET_MIN_WIDTH = '560px'
 export const DESKTOP_MIN_WIDTH = '1024px'
 
 export const TOP_HEADER_HEIGHT = '3rem'

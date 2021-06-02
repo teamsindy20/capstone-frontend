9c138c04818c105cbf138e3bed120062e2b13ecc
HyojinKim <gywls00100@gmail.com>
2021-06-02 15:45:49 +0900

    Add location page

---
+++ src/pages/location/index.tsx
@@ -0,0 +1,8 @@
+import PageHead from 'src/components/layouts/PageHead'
+import styled from 'styled-components'
+
+function Location() {
+  return <PageHead>주소변경</PageHead>
+}
+
+export default Location

64b092f23277bb0682bc8b5b1e3206ef8e348c11
HyojinKim <gywls00100@gmail.com>
2021-06-03 14:40:52 +0900

    Cjange header color

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -15,13 +15,11 @@ const FixedHeader = styled.header`
   position: fixed;
   bottom: 0;
   z-index: 1;
-
   width: 100%;
   max-width: ${TABLET_MIN_WIDTH};
   height: ${HEADER_HEIGHT};
-
-  background-color: #ff9a87;
-  box-shadow: 0 0 0 1px rgba(16, 22, 26, 0.15), 0 0 0 rgba(16, 22, 26, 0), 0 0 0 rgba(16, 22, 26, 0);
+  box-shadow: 0 -2px 5px 0 rgba(142, 142, 142, 0.25);
+  background-color: #ffffff;
 `
 
 const FlexContainerAroundNav = styled.nav`
@@ -48,7 +46,7 @@ const NoMarginH6 = styled.h5`
   margin: 0;
 `
 
-const StyledHomeRoundedIcon = { fontSize: 30, color: grey[800] }
+const StyledHomeRoundedIcon = { fontSize: 28, color: '#ff9a88' }
 
 function Header() {
   return (
@@ -62,26 +60,26 @@ function Header() {
         </ClientSideLink>
         <ClientSideLink href="/feed">
           <FlexContainerColumnCenterCenter>
-            <StoreRoundedIcon style={{ fontSize: 30, color: grey[800] }} />
+            <StoreRoundedIcon style={{ fontSize: 28, color: '#ff9a88' }} />
             <NoMarginH6>소식</NoMarginH6>
           </FlexContainerColumnCenterCenter>
         </ClientSideLink>
         <ClientSideLink href={`/users/${username}/favorite-menus`}>
           <FlexContainerColumnCenterCenter>
-            <FavoriteRoundedIcon style={{ fontSize: 30, color: grey[800] }} />
+            <FavoriteRoundedIcon style={{ fontSize: 25, color: '#ff9a88' }} />
             <NoMarginH6>찜</NoMarginH6>
           </FlexContainerColumnCenterCenter>
         </ClientSideLink>
         <ClientSideLink href={`/users/${username}/orders`}>
           <FlexContainerColumnCenterCenter>
-            <AssignmentTwoToneIcon style={{ fontSize: 30, color: grey[800] }} />
+            <AssignmentTwoToneIcon style={{ fontSize: 28, color: '#ff9a88' }} />
             <NoMarginH6>주문내역</NoMarginH6>
           </FlexContainerColumnCenterCenter>
         </ClientSideLink>
         <ClientSideLink href={`/users/${username}`}>
           <FlexContainerColumnCenterCenter>
-            <PersonRoundedIcon style={{ fontSize: 30, color: grey[800] }} />
-            <NoMarginH6>마이페이지</NoMarginH6>
+            <PersonRoundedIcon style={{ fontSize: 28, color: '#ff9a88' }} />
+            <NoMarginH6>MY</NoMarginH6>
           </FlexContainerColumnCenterCenter>
         </ClientSideLink>
       </FlexContainerAroundNav>

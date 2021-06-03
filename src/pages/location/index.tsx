3f22cd01e394acfc769d5f205b4cf4e1838996f4
HyojinKim <gywls00100@gmail.com>
2021-06-03 15:36:45 +0900

    Add address div

--- src/pages/location/index.tsx
+++ src/pages/location/index.tsx
@@ -5,6 +5,8 @@ import { FlexContainerBetween, FlexContainerAlignCenter } from 'src/styles/FlexC
 import ArrowBackIosRoundedIcon from '@material-ui/icons/ArrowBackIosRounded'
 import grey from '@material-ui/core/colors/grey'
 import useGoBack from 'src/hooks/useGoBack'
+import LocationOnRoundedIcon from '@material-ui/icons/LocationOnRounded'
+import { Button, Input, Space, Divider, Modal } from 'antd'
 
 const FlexContainerBetween1 = styled(FlexContainerBetween)`
   height: 100%;
@@ -17,6 +19,67 @@ const WhiteText = styled.h5`
   color: #ffffff;
 `
 const StyledArrowBackIosRoundedIcon = { fontSize: 20, color: grey[800] }
+
+const StyledLocationOnRoundedIcon = styled(LocationOnRoundedIcon)`
+  color: #ff9a88;
+  font-size: 16px;
+  margin: 0;
+`
+
+const GridContainer = styled.div`
+  display: grid;
+  margin: 1.2rem;
+  grid-template-row: auto;
+`
+
+const TitleText = styled.h3`
+  font-size: 18px;
+  font-weight: bold;
+  margin-bottom: 18px;
+`
+const AdressText = styled.h3`
+  font-size: 16px;
+  font-weight: 500;
+`
+const Adress2Text = styled(AdressText)`
+  color: #7c7c7c;
+`
+const AddresContainer = styled.div`
+  display: flex;
+`
+const Adress2Icon = styled.div`
+  width: 54px;
+  height: 26px;
+  border-radius: 13px;
+  border: solid 1px #f57861;
+  background-color: #ffffff;
+  color: #f57861;
+  font-size: 13px;
+  font-weight: 500;
+  text-align: center;
+  margin: 0 6px 0 0;
+`
+
+const PinkButton = styled.button`
+  background-color: white;
+  color: #2eccba;
+  border: 1px solid #2eccba;
+  text-align: center;
+  text-decoration: none;
+  padding: 0.5em 0.5rem;
+  font-size: 1rem;
+  margin: 1px 1px;
+  border-radius: 0.3rem;
+  cursor: pointer;
+  display: inline-block;
+  transition-duration: 0.4s;
+
+  &:hover {
+    background-color: #2eccba;
+    border: white;
+    color: white;
+  }
+`
 
 function Location() {
   const goBack = useGoBack()
@@ -32,6 +95,17 @@ function Location() {
           <WhiteText>ㅇ</WhiteText>
         </FlexContainerBetween1>
       </TopHeader>
+      <GridContainer>
+        <TitleText>현주소</TitleText>
+        <AddresContainer>
+          <StyledLocationOnRoundedIcon />
+          <AdressText>동작구 흑석동 221 208관 1층</AdressText>
+        </AddresContainer>
+        <AddresContainer>
+          <Adress2Icon>도로명</Adress2Icon>
+          <Adress2Text>흑석로 84 208관 1층</Adress2Text>
+        </AddresContainer>
+      </GridContainer>
     </PageHead>
   )
 }

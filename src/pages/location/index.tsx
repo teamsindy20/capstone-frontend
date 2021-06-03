38c6b7d5ea3f8840d898dab5f34bf8179a7f420d
HyojinKim <gywls00100@gmail.com>
2021-06-03 14:31:56 +0900

    Add TopHeader

--- src/pages/location/index.tsx
+++ src/pages/location/index.tsx
@@ -1,8 +1,39 @@
 import PageHead from 'src/components/layouts/PageHead'
 import styled from 'styled-components'
+import TopHeader from 'src/components/TopHeader'
+import { FlexContainerBetween, FlexContainerAlignCenter } from 'src/styles/FlexContainer'
+import ArrowBackIosRoundedIcon from '@material-ui/icons/ArrowBackIosRounded'
+import grey from '@material-ui/core/colors/grey'
+import useGoBack from 'src/hooks/useGoBack'
+
+const FlexContainerBetween1 = styled(FlexContainerBetween)`
+  height: 100%;
+`
+const FlexContainerCenterCenter = styled(FlexContainerAlignCenter)`
+  justify-content: center;
+  height: 100%;
+`
+const WhiteText = styled.h5`
+  color: #ffffff;
+`
+const StyledArrowBackIosRoundedIcon = { fontSize: 20, color: grey[800] }
 
 function Location() {
-  return <PageHead>주소변경</PageHead>
+  const goBack = useGoBack()
+
+  return (
+    <PageHead>
+      <TopHeader>
+        <FlexContainerBetween1>
+          <FlexContainerAlignCenter>
+            <ArrowBackIosRoundedIcon style={StyledArrowBackIosRoundedIcon} onClick={goBack} />
+          </FlexContainerAlignCenter>
+          <FlexContainerCenterCenter>주소지 설정</FlexContainerCenterCenter>
+          <WhiteText>ㅇ</WhiteText>
+        </FlexContainerBetween1>
+      </TopHeader>
+    </PageHead>
+  )
 }
 
 export default Location

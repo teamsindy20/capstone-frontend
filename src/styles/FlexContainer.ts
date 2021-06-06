221f48f5293fd7ab17971d9d82d1c7fa07a23d8b
HyojinKim <gywls00100@gmail.com>
2021-06-06 22:19:30 +0900

    Delete font size, weight

--- src/styles/FlexContainer.ts
+++ src/styles/FlexContainer.ts
@@ -3,8 +3,8 @@ import styled from 'styled-components'
 export const FlexContainerAlignCenter = styled.div`
   display: flex;
   align-items: center;
-  font-size: 16px;
-  font-weight: 500;
+  //font-size: 16px;
+  //font-weight: 500;
 `
 
 export const FlexContainerBetween = styled.div`

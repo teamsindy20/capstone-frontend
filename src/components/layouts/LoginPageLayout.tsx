3259170e738282d010f7f3ba234ebbe83a3af18a
Taeuk Gwak <gwak2837@kakao.com>
2021-06-07 18:06:11 +0900

    Add link

--- src/components/layouts/LoginPageLayout.tsx
+++ src/components/layouts/LoginPageLayout.tsx
@@ -2,6 +2,7 @@ import Image from 'next/image'
 import { ReactNode } from 'react'
 import { FlexContainerAlignCenter } from 'src/styles/FlexContainer'
 import styled from 'styled-components'
+import ClientSideLink from '../atoms/ClientSideLink'
 
 const Padding = styled.div`
   padding: 1rem;
@@ -27,20 +28,20 @@ function LoginPageLayout({ children }: Props) {
   return (
     <>
       <Padding />
-
-      <FlexContainerColumn>
-        <Image
-          src="/dessert-fit-logo-white.png"
-          alt="dessert-fit-logo-white.png"
-          width="124"
-          height="124"
-        />
-        <LogoTextImg src="/dessert-fit-text.png" alt="dessert-fit-text.png" />
-        <h4>어? 나 디저트 좋아하네</h4>
-        <h4>나만의 디저트를 핏하다</h4>
-      </FlexContainerColumn>
+      <ClientSideLink href="/">
+        <FlexContainerColumn>
+          <Image
+            src="/dessert-fit-logo-white.png"
+            alt="dessert-fit-logo-white.png"
+            width="124"
+            height="124"
+          />
+          <LogoTextImg src="/dessert-fit-text.png" alt="dessert-fit-text.png" />
+          <h4>어? 나 디저트 좋아하네</h4>
+          <h4>나만의 디저트를 핏하다</h4>
+        </FlexContainerColumn>
+      </ClientSideLink>
       {children}
-
       <Padding />
     </>
   )
